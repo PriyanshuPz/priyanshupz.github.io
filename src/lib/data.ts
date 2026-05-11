495b43e4732f7cbada532528e76f8ea91472a986
Priyanshu Verma <priyanshuverma@outlook.in>
2026-05-11 08:26:13 +0000

    redirecting to new place
    Signed-off-by: Priyanshu Verma <priyanshuverma@outlook.in>

--- src/lib/data.ts
+++
@@ -1,8 +0,0 @@
-export const PRESENT = {
-  project: {
-    name: "💛 OPENFLAG",
-    description:
-      "A simple social app but not like normal social media. It is made for people who actually build things developers, designers, makers and want to show real work instead of just posting random content.",
-    url: "https://openflag.xyz",
-  },
-};
--- src/lib/layouts/base.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  const { children, ...rest } = $props();
-</script>
-
-<article class="prose prose-sm md:prose-base dark:prose-invert">
-  {@render children()}
-</article>
